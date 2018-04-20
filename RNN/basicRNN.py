0d66129565cc68bce4cc3feda51c02835a34036b
coffee <yafei_kou@163.com>
2018-04-20 11:06:53 +0800

    add RNN

--- RNN/basicRNN.py
+++ RNN/basicRNN.py
@@ -3,3 +3,15 @@
    Author :       kouyafei
    date：          2018/4/20
 """
+
+def tanh():
+    pass
+
+def rnn_cell_forward():
+    pass
+
+def rnn_cell_backward():
+    pass
+
+def loss():
+    pass
